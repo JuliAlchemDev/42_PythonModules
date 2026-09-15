efc7553d49868bb1892830ab4b643a1d2d4ca457
Iuliia Alkhimova <iualkhim@student.42barcelona.com>
2026-09-15 11:55:03 +0200

    feat: implement basic plant program structure

---
+++ mod01/ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    plant = "Rose"
+    height = 25
+    age = 30
+    print(
+        "=== Welcome to My Garden ===",
+        f"Plant: {plant}",
+        f"Height: {height}cm",
+        f"Age: {age} days",
+        "",
+        "=== End of Program ===",
+        sep="\n"
+        )
+
+
+if __name__ == "__main__":
+    main()
